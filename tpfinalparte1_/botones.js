67a638476fd13bedcc72748a9c705d92417e29ac
Rossi Francisca <167583183+sherifflizard@users.noreply.github.com>
2024-10-17 15:06:58 -0300

    Update botones.js

--- tpfinalparte1_/botones.js
+++ tpfinalparte1_/botones.js
@@ -1,1 +1,18 @@
 
+//posXBoton = anchoPantalla * 0.9;
+//posYBoton = altoPantalla * 0.9;
+//anchoBoton = anchoPantalla * 0.05;
+//altoBoton = altoPantalla * 0.05;
+
+
+//function botoncitosig(){
+//if(
+//    mouseX > posXBoton && 
+//    mouseX < posXBoton+anchoBoton && 
+//    mouseY > posYBoton && 
+//    mouseY < posYBoton+altoBoton){
+//  }
+//}
+
+//}
+
