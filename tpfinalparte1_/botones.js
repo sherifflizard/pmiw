89e6c0d52d7e10595520d0918e29d9f7e020fd3b
Rossi Francisca <167583183+sherifflizard@users.noreply.github.com>
2024-10-17 21:58:48 -0300

    Add files via upload

---
+++ tpfinalparte1_/botones.js
@@ -0,0 +1,21 @@
+////BOTONES
+
+//let posXBoton;
+//let posYBoton;
+
+//posXBoton = anchoPantalla * 0.9;
+//posYBoton = altoPantalla * 0.9;
+//anchoBoton = anchoPantalla * 0.05;
+//altoBoton = altoPantalla * 0.05;
+
+
+//function botoncitosig(){
+//if(
+//    mouseX > posXBoton && 
+//    mouseX < posXBoton+anchoBoton && 
+//    mouseY > posYBoton && 
+//    mouseY < posYBoton+altoBoton){
+//  }
+//}
+
+//}
