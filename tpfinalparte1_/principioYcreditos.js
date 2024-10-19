d63fa26eedba0d7eda412c708ac9bcc4d8e04b08
Rossi Francisca <167583183+sherifflizard@users.noreply.github.com>
2024-10-18 23:05:12 -0300

    Update principioYcreditos.js

--- tpfinalparte1_/principioYcreditos.js
+++ tpfinalparte1_/principioYcreditos.js
@@ -5,13 +5,29 @@ function pantallaempezar() {
   if (imagenes[0]) {
     image(imagenes[0], 0, 0, width, height);
   }
-  fill(0, 100, 100, 100);
-  rect (0, 0, 700, 700);
-  fill(50, 50, 0);
+  fill(0,100,100,100);
+  rect (0,0,700,700);
+  fill(50,50,0);
   textAlign(CENTER);
   textSize(40);
-  stroke(100);
+  strokeWeight(100);
   text("Laputa,el castillo en el cielo", width / 2, height / 2);
+  //dibujaBoton("JUGAR", width / 2, height * 0.75, 200, 40);
+  //dibujaBoton("CRÉDITOS", width / 2, height * 0.75 + 60, 200, 40);
+
+  pop();
+}
+
+function creditos() {
+  push();
+  background(255, 0, 0);
+
+ 
+  fill(50,50,0);
+  textAlign(CENTER);
+  textSize(40);
+  strokeWeight(100);
+  text("creditos", width / 2, height / 2);
   //dibujaBoton("JUGAR", width / 2, height * 0.75, 200, 40);
   //dibujaBoton("CRÉDITOS", width / 2, height * 0.75 + 60, 200, 40);
 
