a640a77536a1480cfc1c6edc4a4e489f6fba0fce
Christian123456789101 <166933386+Christian123456789101@users.noreply.github.com>
2024-10-19 23:02:07 -0300

    Add files via upload

---
+++ tpfinalparte1_/funciones.js
@@ -0,0 +1,199 @@
+
+//--------------------------
+
+function reload() {
+  anchoTexto = anchoPantalla * 0.8;
+  tamTexto = altoPantalla * 0.04;
+  inicioTexto = 10;
+
+  textSize(tamTexto);
+}
+
+function texto() {
+  push();
+  textAlign(CENTER);
+  textSize(tamTexto);
+  fill(255);
+  text(misTextos[indicePantalla], 50, 10, anchoTexto, 20*8);// Corrección 20*8 = textSize * 8
+  pop();
+}
+
+//-------
+function mouseClicked() {
+  posTextoActual++;
+}
+
+//-------
+function obtenerTextActual() {
+  return posTextoActual;
+}
+
+
+//______________
+
+//usar para pantallas de siguiente, c/ parametros asi se puede reeutilizar jijiji
+function botonReinicio() {
+  push();
+  rectMode(CENTER);
+  fill(0, 200);
+  rect(botonPosX, botonPosY, botonAnchura, botonAltura);
+  fill(255);
+  textAlign(CENTER);
+  textSize(16);
+  text("REINICIAR", botonPosX, botonPosY);
+  pop();
+}
+
+function boton() {
+  push();
+  rectMode(CENTER);
+  fill(0, 200);
+  rect(botonPosX, botonPosY, botonAnchura, botonAltura);
+  fill(255);
+  textAlign(CENTER);
+  textSize(16);
+  text("SIGUIENTE", botonPosX, botonPosY);
+  pop();
+}
+
+function dosBotones() {
+  push();
+  rectMode(CENTER);
+  fill(0, 200);
+  rect(botonPosX, botonPosY, botonAnchura, botonAltura);
+  rect(botonPosX, botonPosY2, botonAnchura, botonAltura);
+  fill(255);
+  textAlign(CENTER);
+  textSize(16);
+  text("SIGUIENTE1", botonPosX, botonPosY);
+  text("SIGUIENTE2", botonPosX, botonPosY2);
+  pop();
+}
+
+function pantallaUnBoton() {
+  background(255, 255, 0);
+  if (indicePantalla < imagenes.length) {
+    image(imagenes[indicePantalla], 0, 0, width, height);
+    fill(0);
+    textAlign(CENTER);
+    textSize(20);
+    text("Pantalla" + indicePantalla, width / 2, height / 2);
+    texto();
+  }
+  boton();
+}
+
+function pantallaDosBotones() {
+  background(255, 255, 0);
+  if (indicePantalla < imagenes.length) {
+    image(imagenes[indicePantalla], 0, 0, width, height);
+    fill(0);
+    textAlign(CENTER);
+    textSize(20);
+    text("Pantalla" + indicePantalla, width / 2, height / 2);// Dibuja la imagen correspondiente
+    texto();
+  }
+  dosBotones();
+}
+
+
+
+//pantallas
+
+function pantalla1() {
+  pantallaUnBoton();
+}
+
+function pantalla2() {
+  pantallaDosBotones();
+}
+
+function pantalla3() {
+  pantallaUnBoton();
+}
+
+function pantalla4() {
+  pantallaUnBoton();
+}
+
+function pantalla5() {
+  pantallaDosBotones();
+}
+
+function pantalla6() {
+  pantallaDosBotones();
+}
+
+function pantalla7() {
+  pantallaDosBotones();
+}
+
+function pantalla8() {
+  pantallaUnBoton();
+}
+
+function pantalla9() {
+  pantallaDosBotones();
+}
+
+function pantalla10() {
+  pantallaUnBoton();
+}
+
+function pantalla11() {
+  pantallaUnBoton();
+}
+
+function pantalla12() {
+  pantallaUnBoton();
+}
+
+function pantalla13() {
+  pantallaUnBoton();
+}
+
+function pantalla14() {
+  pantallaDosBotones();
+}
+
+function pantalla15() {
+  background(255, 255, 0);
+  if (indicePantalla < imagenes.length) {
+    image(imagenes[indicePantalla], 0, 0, width, height);
+    fill(0);
+    textAlign(CENTER);
+    textSize(20);
+    text("Pantalla" + indicePantalla, width / 2, height / 2);
+    textSize(tamTexto);
+    text(misTextos[indicePantalla], 0, 0);
+  }
+  botonReinicio();
+}
+
+function pantalla16() {
+  pantallaUnBoton();
+}
+
+function pantalla17() {
+  pantallaUnBoton();
+}
+
+function pantalla18() {
+  pantallaUnBoton();
+}
+
+function pantalla19() {
+  pantallaDosBotones();
+}
+
+function pantalla20() {
+  pantallaUnBoton();
+}
+
+function pantalla21() {
+  pantallaDosBotones();
+}
+
+function pantalla22() {
+  pantallaUnBoton();
+}
---
+++ tpfinalparte1_/principioYcreditos.js
@@ -0,0 +1,25 @@
+function pantallaempezar() {
+  background(255, 0, 0);
+  pantallaUnBoton();
+  fill(150,100,150);
+  textAlign(CENTER);
+  textSize(32);
+  text("LAPUTA \nEL CASTILLO EN EL CIELO", width / 2, height / 2);
+  boton();
+}
+
+function creditos() {
+  push();
+  background(255, 0, 0);
+
+ 
+  fill(50,50,0);
+  textAlign(CENTER);
+  textSize(40);
+  strokeWeight(100);
+  text("creditos", width / 2, height / 2);
+  //dibujaBoton("JUGAR", width / 2, height * 0.75, 200, 40);
+  //dibujaBoton("CRÉDITOS", width / 2, height * 0.75 + 60, 200, 40);
+
+  pop();
+}
